2a32563231b975d83ad4c9227d7e5ea2c78a99a3
madbull12 <huangandrian02@gmail.com>
2022-12-30 00:59:24 +0700

    schema changes

--- pages/favourites.tsx
+++ pages/favourites.tsx
@@ -1,13 +1,25 @@
+import { Prisma } from "@prisma/client";
 import React from "react";
 import Body from "../components/Body";
+import Poster from "../components/Poster";
+import Saved from "../components/Saved";
 import useFavourites from "../hooks/useFavourites";
+import { v4 } from "uuid"
 
 const FavouritePage = () => {
     const { userFavourites } = useFavourites();
     console.log(userFavourites)
   return (
     <Body>
-        
+        <h1 className="text-3xl font-bold">Favourites</h1>
+        <div className='row  scrollbar-thumb-gray-800 scrollbar-thin scrollbar-track-gray-none rounded scrollbar-thumb-rounded-md'>
+          {userFavourites?.map((movie)=>(
+                
+            <Saved movie={movie} key={v4()} />
+   
+          ))}
+        </div>
+      
     </Body>
   );
 };
